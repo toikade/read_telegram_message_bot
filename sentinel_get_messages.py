e7f7a537e591df246d4768b5728a169888d2cb4d
Aaron <toikadeaaron@gmail.com>
2024-03-10 19:50:59 +0100

    modified sentinel_crypto to get date and message for yesterday

--- sentinel_get_messages.py
+++ sentinel_get_messages.py
@@ -1,5 +1,6 @@
 import telethon
 from telethon.sync import TelegramClient, events
+from datetime import date, timedelta
 import datetime
 import re
 
@@ -11,8 +12,12 @@ bot_token = '6379620803:AAEaLOHQM6Zeo3niZFDDDjS4NnkH1S2NqqM'  #'your_bot_token'
 with TelegramClient('test', api_id, api_hash) as client:
     chat_entity = client.get_input_entity('t.me/SentinelCrypto')
     print(chat_entity.channel_id)
+    #get the date of yesterday
+    yesterday = date.today() - timedelta(days = 1)
+    #get today's date
+    today = datetime.date.today()
     #get the messages for today
-    for message in client.iter_messages(chat_entity, offset_date=datetime.date.today(), reverse=True):
+    for message in client.iter_messages(chat_entity, offset_date=yesterday, reverse=True):
         #dictionary where all scraped data will be stored
         # print(type(message.text))
         test_msg = "['Binance Futures, ByBit USDT, KuCoin Futures, OKX Futures', '#ENS/USDT Closed at stoploss after reaching take profit ⚠']"
